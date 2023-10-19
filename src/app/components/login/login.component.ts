8ba92015fd9dc6c499338100dd42676ce8c91b3d
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-19 22:44:39 +0400

    implement log in and log out

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  AngularFireAuth,
+  AngularFireAuthModule,
+} from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  constructor(private afAuth: AngularFireAuth) {}
+  async login() {
+    try {
+      const result = await this.afAuth.signInWithPopup(
+        new firebase.auth.GoogleAuthProvider()
+      );
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  user: firebase.User;
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  async logout() {
+    try {
+      const result = await this.afAuth.signOut();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
