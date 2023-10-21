0c41cf11fb5202bffe224f90187b76543fa9a0f6
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-21 18:45:30 +0400

    implement all crud operations for admins

--- src/app/components/admin/product-form/product-form.component.ts
+++ src/app/components/admin/product-form/product-form.component.ts
@@ -20,6 +20,7 @@ export class ProductFormComponent {
     category: '',
     imageUrl: '',
   };
+  id;
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
@@ -28,10 +29,10 @@ export class ProductFormComponent {
   ) {
     this.categories$ = categoryService.getCategories();
 
-    let id = this.route.snapshot.paramMap.get('id');
-    if (id) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
       this.productService
-        .get(id)
+        .get(this.id)
         .pipe(take(1))
         .subscribe((p) => (this.product = p));
     }
@@ -49,7 +50,13 @@ export class ProductFormComponent {
     ]),
   });
   save(product) {
-    this.productService.create(product);
+    if (this.id) this.productService.update(this.id, product);
+    else this.productService.create(product);
+    this.router.navigate(['/admin/products']);
+  }
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) return;
+    this.productService.delete(this.id);
     this.router.navigate(['/admin/products']);
   }
 }
