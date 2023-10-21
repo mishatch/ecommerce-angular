85bd777accc1f70852c29707f02fa9138ffd1c4f
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-21 22:56:10 +0400

    add categories for shop

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,21 +2,40 @@ import { Component } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
-  products$;
+  products: any[] = [];
+  filteredProducts: any[];
   categories$;
+  category: string;
+
   constructor(
     productService: ProductService,
     categoryService: CategoryService,
     private route: ActivatedRoute
   ) {
-    this.products$ = productService.getAll();
+    productService
+      .getAll()
+      .pipe(
+        switchMap((products) => {
+          this.products = products;
+          return this.route.queryParamMap;
+        })
+      )
+      .subscribe((params) => {
+        this.category = params.get('category');
+
+        this.filteredProducts = this.category
+          ? this.products.filter((p) => p.category === this.category)
+          : this.products;
+      });
+
     this.categories$ = categoryService.getCategories();
-    console.log(this.route.snapshot.queryParamMap.get('category'));
   }
 }
