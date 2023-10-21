724fbb5b5b7ed70d68a48b9b32566bf092894e86
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-21 19:34:34 +0400

    fix admin-products route error

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,28 @@
 import { ProductService } from './../../../services/product.service';
-import { Component } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss'],
 })
-export class AdminProductsComponent {
-  products$;
+export class AdminProductsComponent implements OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: any;
   constructor(private ProductService: ProductService) {
-    this.products$ = ProductService.getAll();
+    this.subscription = this.ProductService.getAll().subscribe((products) => {
+      this.filteredProducts = this.products = products;
+    });
+  }
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter((p) =>
+          p.title.toLowerCase().includes(query.toLowerCase())
+        )
+      : this.products;
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
