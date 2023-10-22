a023d665166ea9889251552829e528fad6ca8523
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-23 00:43:57 +0400

    add products to cart on database

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -14,14 +14,6 @@ export class ProductCardComponent {
   constructor(private cartService: ShoppingCartService) {}
 
   addToCart(product: Product) {
-    let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      this.cartService.create().then((result) => {
-        localStorage.setItem('cartId', result.key);
-        //add
-      });
-    } else {
-      //add
-    }
+    this.cartService.addToCart(product);
   }
 }
