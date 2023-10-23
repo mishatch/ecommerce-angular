d4f36483459b50b0c0ae549850f97eeab5747de9
Misha Tchanturidze <tchanturidzemikheili@gmail.com>
2023-10-23 16:27:53 +0400

    add total amount and item prices to shopping cart

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -5,7 +5,18 @@ export class ShoppingCart {
   constructor(
     public itemsMap: { [key: string]: ShoppingCartItem } | ShoppingCartItem[]
   ) {
-    for (let productId in itemsMap) this.items.push(itemsMap[productId]);
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items) {
+      sum += this.items[productId].totalPrice;
+    }
+    return sum;
   }
 
   get totalItemsCount() {
